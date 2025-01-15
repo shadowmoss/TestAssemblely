fc531522551667735f347806abea17acc750b9e4
shadowmoss <shadowmoss@github.com>
2025-01-14 22:15:40 -0800

    深入理解计算机.3.11节。第二章需要重新看一遍

---
+++ csapp/chapter03/proc.s
@@ -0,0 +1,19 @@
+	.file	"proc.c"
+	.text
+	.globl	proc
+	.type	proc, @function
+proc:
+.LFB0:
+	.cfi_startproc
+	movq	16(%rsp), %rax
+	addq	%rdi, (%rsi)
+	addl	%edx, (%rcx)
+	addw	%r8w, (%r9)
+	movl	8(%rsp), %edx
+	addb	%dl, (%rax)
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	proc, .-proc
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-44)"
+	.section	.note.GNU-stack,"",@progbits
